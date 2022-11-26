938639d3661e61fae5e9609e5d87c9296d33cced
Anthony Hansord <tonyhansorddev@gmail.com>
2022-11-26 15:11:40 +1100

    done

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then((resp) => resp.json())
+    .then((json) => renderBooks(json));
+}
+
+function fetchCharacters() {
+  fetch("https://anapioficeandfire.com/api/characters/1031")
+    .then((resp) => resp.json())
+    .then((json) => console.log(json));
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
+  let pages = 0;
+  console.log(`The 5th book is ${books[4].name}`);
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    pages += book["numberOfPages"];
+    const h2 = document.createElement("h2");
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
+  console.log(pages);
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener("DOMContentLoaded", function () {
   fetchBooks();
+  fetchCharacters();
 });
